3ae71910d41bdaae36c0dd840698bbe6db8b57a6
gunsleobezerra <you@example.com>
2018-08-25 12:52:08 -0300

    Atualizando função inserir

--- main.c
+++ main.c
@@ -6,30 +6,40 @@
 #define tam 1000
 
 typedef struct liv{
- char  ISBN[13],title[30],author[30],year[4];
-
+ char  ISBN[14],title[50],author[50],year[5];
+	int next;
 }Livro;
 
 void openFile(FILE **fil,char *filname);
 void insertFile(FILE *fil);
 void dumpFile(FILE *fil);
+void hashSfile(FILE *fil,int size_data,Livro book,int old);
 
 int main()
 {
     char opc;
-    FILE *file;
+    FILE *file=NULL;
     char filename[100];
 
     do{
         system("cls");
-        printf("HELLO MOTHERFUCK!\n");
-        printf("[1]-INSERT INTO FILE\ne[4]-Load FILE\n[5]-Dump FILE\n[e]-Close FILE & exit \n");
+        printf("FILE MENAGER\n\n\n");
+        printf(" [1]-INSERT INTO FILE\n\n [4]-Load FILE\n\n [5]-Dump FILE\n\n [e]-Close FILE & exit \n");
         opc=getch();
 
         switch(opc){
             case '1':
-                insertFile(file);
-              break;
+            	
+            	if(file){
+				
+                	insertFile(file);
+          			break;
+				}
+				system("cls");
+				printf("File is not defined!!\n\nPRESS ANY BUTTON");
+				getch();
+				
+			  break;
 
             case '4':
                 system("cls");
@@ -58,24 +68,7 @@ void openFile(FILE **fil,char *filname){
         Sleep(1000);
 
 
-        printf(".");
-         Sleep(500);
-
-        printf(".");
-         Sleep(1000);
-
-        printf(".");
-         Sleep(300);
-         printf(".");
-         Sleep(500);
-
-        printf(".");
-         Sleep(1000);
-
-        printf(".");
-         Sleep(300);printf(".");
-         Sleep(500);
-
+       
         printf(".");
          Sleep(1000);
 
@@ -91,6 +84,12 @@ void openFile(FILE **fil,char *filname){
 
         printf(".");
         *fil = fopen(filname,"a+");
+   
+    /*inicia lista de posições disponiveis vázia */
+		
+		fprintf(*fil,"%d",-1);
+        
+        
     }
 
 }
@@ -114,8 +113,11 @@ void dumpFile(FILE *fil){
 
 void insertFile(FILE* fil){
     Livro book;
-    int regSize;
-    printf("ISBN:");
+    int regSize,list;
+    
+    system("cls");
+    
+	printf("ISBN:");
     gets(book.ISBN);
     printf("\nTitle:");
     gets(book.title);
@@ -124,7 +126,44 @@ void insertFile(FILE* fil){
     printf("\nYear:");
     gets(book.year);
     regSize=strlen(strcat(strcat(strcat(book.author,book.ISBN),book.title),book.year));
-    printf("REG SIZE:%d\n",regSize);
-    getch();
+    rewind(fil);
+    fscanf(fil,"%d",&list);
+    if(list==-1){
+		fseek(fil,SEEK_END,SEEK_CUR);
+		hashSfile(fil,regSize,book,0);
+		return;
+	}
+	
+	/*BUSCA DE POSIÇÕES DISPONIVEIS*/
+	//implementar
+	
+    
+    
+    
+    
 
+}
+
+void hashSfile(FILE *fil,int size_data,Livro book,int old){
+	
+	
+if(!old) {
+	fprintf(fil,"%d",size_data+3);
+}else{
+	fseek(fil,sizeof(int),SEEK_CUR);
+}
+	
+	fputs(book.ISBN,fil);
+	fputc('#',fil);
+	fputs(book.title,fil);
+	fputc('#',fil);
+	fputs(book.author,fil);
+	fputc('#',fil);
+	fputs(book.year,fil);
+	
+	
+	
+	
+	
+	
 }
